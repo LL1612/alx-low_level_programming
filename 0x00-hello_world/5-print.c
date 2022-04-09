dd370d31d7dc89535f2957504707f2e1782eb122
LL1612 <kilarapy@gmail.com>
2022-04-09 07:24:52 -0700

    printf

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always a (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art")t,\n");
+return (0);
+}
