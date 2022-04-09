e0383b2bfdd3ba46fe1b7135fd78d4d096b3a341
LL1612 <kilarapy@gmail.com>
2022-04-09 07:10:08 -0700

    10

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-
-/**
-*main - Entry point
-*
-*Return; Always 0 (success)
-*/
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return (0);
+#include<stdio.h>
+int main()
+{
+//string intitialisation
+char x[]="Programming is like building a multilingual puzzle";
+int val = puts(x);
+return 0;
 }
