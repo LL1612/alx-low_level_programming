f2f3837c5d67b40b02c830372c15b099f5d7d94d
LL1612 <kilarapy@gmail.com>
2022-04-09 23:30:12 -0700

    final commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-int main() {
-//printf() displays the string inside quotation
-printf("with proper grammer, but the outcome is a piece of art,\n";
-return 0;
-}
+/**
+*main - entry point
+*
