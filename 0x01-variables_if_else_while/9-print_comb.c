12a4e566b4a88b4c6fec749ca1d6cb8955171fdf
LL1612 <kilarapy@gmail.com>
2022-04-11 13:44:55 -0700

    k

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,13 @@
 int main(void)
 {
 	int num;
-	
 	for (num = '0' ; num <= '9' ; num++)
 	{
 		putchar (num);
-		if (num == 9)
+		if (num == 8)
 		{
 			break;
 		}
-	
 	putchar (',');
 	putchar (' ');
 	}
