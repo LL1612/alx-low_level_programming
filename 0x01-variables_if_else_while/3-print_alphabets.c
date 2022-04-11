a8c105fe77a17c36d495f0c486bea69447fd2d99
LL1612 <kilarapy@gmail.com>
2022-04-11 08:51:55 -0700

    1com

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	char small;
+	char cap;
+
+	for (small = 'a' ; small <= 'z' ; small++)
+	for (cap = 'A' ; cap <= 'Z' ; cap++)
+	{
+		putchar(small,cap);
+	}
+	putchar ('\n');
+	return (0);
+}
