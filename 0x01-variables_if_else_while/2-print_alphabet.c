c402f5d850370d3b27075b71d912f9a67e54dbc1
LL1612 <kilarapy@gmail.com>
2022-04-11 05:56:14 -0700

    alp

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,14 @@
  *
  * Return: 0 afetr printing the function
  */
-int main () 
+int main(void)
 {
 	char alp;
 
-	for(alp = 'a' ; alp <= 'z' ; alp++)
+	for (alp = 'a' ; alp <= 'z' ; alp++)
 	{
 		putchar(alp);
+		putchar('\n');
 	}
 
 	return (0);
