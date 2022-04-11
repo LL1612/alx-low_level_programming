66d53b6810f7cbc2922e0d748506da6a4405bea7
LL1612 <kilarapy@gmail.com>
2022-04-11 12:43:31 -0700

    za

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+{
+	char za;
+
+	for (za = 'z' ; za >= 'a' ; za--)
+	{
+		putchar(za);
+	}
+	putchar ('\n');
+	return (0);
+}
