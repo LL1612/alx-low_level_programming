7584d360b7b4c32ca26d03bbbc6f0dbf2c342427
LL1612 <kilarapy@gmail.com>
2022-04-12 12:14:52 -0700

    ll

---
+++ 0x02-functions_nested_loops/4-isapha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: takes in character
+ * Return: 1 if letter, lowercase or uppercase; Return: 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >='a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
