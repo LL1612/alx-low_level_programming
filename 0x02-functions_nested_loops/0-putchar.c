b7ba9e30c265f9f8c30e8efa456b91d768c56e4c
LL1612 <kilarapy@gmail.com>
2022-04-12 07:30:46 -0700

    p

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main -Entry point
+ * _putchar - writes the character c to stdout
  *
  * Return: 0 after printing the function
  */
 int main(void)
 {
-	char n;
-
-	for (n= 'a' ; n <= 'z' ; n++)
-	{
-		putchar(n);
-	}
-	putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
