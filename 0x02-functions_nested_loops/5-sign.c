1ab23bfc7d4c4c6c05ddb4479930fc15e97bf211
LL1612 <kilarapy@gmail.com>
2022-04-12 12:29:35 -0700

    ll1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks alphabetic character
+ * @m: rep character
+ * Return: 1 if letter,lower or uppercase; 0 for otherwise
+ */
+int _isalpha(int m)
+{
+	if ((m >= 'A' && m <= 'Z') || (m >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
