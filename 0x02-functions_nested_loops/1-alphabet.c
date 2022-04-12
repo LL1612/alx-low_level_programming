d4c77e452e167c75c8f4ec390a5319157683dcd7
LL1612 <kilarapy@gmail.com>
2022-04-12 08:01:04 -0700

    az

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char alp;
+
+for (alp = 'a' ; alp <= 'z'; alp++)
+_putchar(alp);
+
+_putchar('\n');
+}
