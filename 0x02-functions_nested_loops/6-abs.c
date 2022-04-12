6334f6441554f8ce4fcf444a22132085fa665bec
LL1612 <kilarapy@gmail.com>
2022-04-12 13:21:50 -0700

    ll3

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computs absolute value
+ * @n:integer
+ *
+ * Return: absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
