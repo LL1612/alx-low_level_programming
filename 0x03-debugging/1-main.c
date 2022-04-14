a775ed9ce886246b8c5fec05f0110a2de44c59b4
LL1612 <kilarapy@gmail.com>
2022-04-14 07:24:57 -0700

    kkj

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -13,11 +11,10 @@ int main(void)
 
 	i = 0;
 
-
 /*
- *while (i < 10)
- *{
- *infinite loop - No increase of variable
+ * while (i < 10)
+ * {
+ * Infinite loop - No increase of variable
  * putchar(i);
  * }
  */
