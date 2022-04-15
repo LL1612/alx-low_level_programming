ff4fb72ac814206a029681b817a48ba05c1c1f90
LL1612 <101508706+LL1612@users.noreply.github.com>
2022-04-15 05:46:15 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -13,14 +13,13 @@ int main(void)
 
 	i = 0;
 
-/*
- * while (i < 10)
- * {
- * Infinite loop - No increase of variable
- * putchar(i);
-}
- */
+/*while (i < 10)*/
+/*{*/
+/* Infinite loop - No increase of variable*/
+/* putchar(i);*/
+/*}*/
 
 printf("infinite loop avoided! \\o/\n");
+	
 return (0);
 }
