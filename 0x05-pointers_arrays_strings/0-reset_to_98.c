a5c39bbb5f9f204b971538cd82287fa94fadaf14
LL1612 <kilarapy@gmail.com>
2022-04-19 02:47:22 -0700

    kl

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98: function takes a pointer to 98
+ * reset_to_98 - function takes a pointer to 98
  * @n: A pointrt to an integer
  *
  * Return: void means correct
