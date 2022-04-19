94e8f4534b24987951355ad993a1cbd5003c137f
LL1612 <kilarapy@gmail.com>
2022-04-19 02:58:28 -0700

    jkhk

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: interger one
+ * @b: intergr two
+ *
+ * Return: always zero
+ */
+
+void swap_int(int *a, int *b);
+{
+	int f:
+		f = *a:
+		*a = *b;
+	*b = f;
+}
