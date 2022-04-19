ed0d5dbb65c333b6ea76a5518cee24ace756896f
LL1612 <kilarapy@gmail.com>
2022-04-19 04:08:37 -0700

    jkj

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 /**
  * _puts - print string
  * @str: pointer to int that will be changed 
+ * 
  * Return: always zero
  */
 
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
 /**
- * _putchar -writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: character to print
+ *
  * Return: Success 1
- * on error, -1 is returned, and error is there returned
+ * on error, -1 is returned, and error is returned
  */
 
 int _putchar(char c)
-{return (write(1, &c, 1));
+{
+	return (write(1, &c, 1));
 }
