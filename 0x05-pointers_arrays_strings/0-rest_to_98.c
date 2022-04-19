985195101cc229843e5372dbbe3e3a16d45bcd76
LL1612 <kilarapy@gmail.com>
2022-04-19 02:30:51 -0700

    lkk

---
+++ 0x05-pointers_arrays_strings/0-rest_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98: function takes a pointer to interger
+ * @n: is an integer
+ *
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+s project is about pointers arrays and strings
