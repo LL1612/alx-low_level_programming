6b4049892d19d9d08d196853ef08ea0af84c7f76
LL1612 <kilarapy@gmail.com>
2022-12-08 06:13:35 -0800

    kjjk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
-int main(void) { fwrite,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 1);
+int main(void) {write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
