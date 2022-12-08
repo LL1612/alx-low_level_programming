d125081189864e2fac87ce554e299bc7cd2ccddb
LL1612 <kilarapy@gmail.com>
2022-12-08 06:41:21 -0800

    jkdie

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-int main(void) {write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+/**
+ * main -prints out the quote
+ * return: 1 if success
+ */
+int main(void) 
+{
+	write(2, "and that piece of art is useful"" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
