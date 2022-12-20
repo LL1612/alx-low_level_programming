9c6efcf8980ada5f880e51c78b9c615560a20696
LL1612 <kilarapy@gmail.com>
2022-12-20 07:50:04 -0800

    kiji

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_rev - a function that prints a string, in reverse
+ *
+ *   * fcounter is to first count to end, n is to count back
+ *
+ *    * @s: str input
+ *
+ *     * Return: string in reverse
+ *
+ *      */
+void rev_string(char *s);
+{
+char alp;
+int i;
+
+i = 0;
+
+while (s[i] != '\0')
+
+{
+i++;
+}
+
+i = i - 1;
+
+for (i = 0; i < i /2; i++)
+{
+	alp = s[i];
+	s[i] = s[i];
+	s[i--] = alp;
+}
+}
