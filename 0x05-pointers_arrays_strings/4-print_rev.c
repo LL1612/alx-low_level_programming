30c8eda885ab7fce7cbbde967a0d4408d0393ae5
LL1612 <kilarapy@gmail.com>
2022-12-20 07:35:22 -0800

    kii

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,17 @@
 
 void print_rev(char *s)
 {
-	int i;
-	i = 0;
+int i;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	for (i = i - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (i = i - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
