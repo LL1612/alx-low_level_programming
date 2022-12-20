8d5ef5881598d470063dcbf1e8cd6c7ee8f2b2f1
LL1612 <kilarapy@gmail.com>
2022-12-20 06:29:41 -0800

    sdf

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,20 @@
  */
 
 void print_rev(char *s);
-
-int len = 0;
-	
-	int i, n, len;
-	for (i = 0; s[i]; != '\0'; i++)
-{{
-	len++;
-}
-for (n = (len -1); n >= 0; n--)
 {
-	_putchar(s[n]);
-}
-_putchar('\n');
+	int i, n, len;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (n = len - 1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
 }
