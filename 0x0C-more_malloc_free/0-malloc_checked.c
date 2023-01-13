1c04efeb330195480ac9c287abc09b42bd143eee
LL1612 <kilarapy@gmail.com>
2023-01-13 01:04:22 -0800

    last

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * malloc_checked - prints a string
  * @b:number of memory
+ *
  * Return: void
  */
 
@@ -11,9 +13,8 @@ void *malloc_checked(unsigned int b)
 void *p;
 
 p = malloc(b);
-if (p == null)
-{
+if (p == NULL)
 exit(98);
-}
+
 return (p);
 }
