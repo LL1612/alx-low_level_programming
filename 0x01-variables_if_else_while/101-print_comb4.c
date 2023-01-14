e9f159282b60898822d9a7f4ba3e4c1412cef342
LL1612 <kilarapy@gmail.com>
2023-01-13 21:54:07 -0800

    iji

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+int num1, num2, num3;
+int arr[3];
+for (num1 = 0; num1 < 9; num1++)
+{
+for (num2 = 0; num2 < 10; num2++)
+{
+for (num3 = 0; num3 < 10; num3++)
+{
+putchar((num1 % 10) + '0');
+putchar((num2 % 10) + '0');
+putchar((num3 % 10) + '0');
+if (num1 != num2 && num2 != num3 && num3 != num1)
+	continue;
+putchar(',');
+putchar(' ');
+}}}
+putchar('\n');
+return (0);
+}
