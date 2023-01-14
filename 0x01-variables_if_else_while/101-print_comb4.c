a860b306432f1f09428c61df166331fbbd225f30
LL1612 <kilarapy@gmail.com>
2023-01-13 23:11:55 -0800

    jiji

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ for (num3 = 2; num3 < 10; num3++)
 putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
 putchar((num3 % 10) + '0');
-if (num1 == 7 && num2 == 8 && num3 == 9)
+if (num1 != num2 && num2 != num3 && num3 != num1)
 	continue;
 putchar(',');
 putchar(' ');
